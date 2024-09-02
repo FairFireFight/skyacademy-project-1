dc5b8e33f9973edb384bdbe8fef778933ee2f4a0
Sulaiman <sulaimanallahham83@gmail.com>
2024-09-02 16:38:56 +0300

    started requests card

--- libs/js/app.js
+++ libs/js/app.js
@@ -9,7 +9,8 @@ and executed on every page in this project.
 
 Contents:
     1. Global function declarations
-    2. Navbar loader 
+    2. Preload checks
+    3. Navbar loader 
 */
 
 
@@ -26,29 +27,36 @@ async function getComponent(path) {
     return data;
 }
 
-// 2. Navbar Loader =======================================
+// 2. Preload checks ======================================
+if (!window.location.host) {
+    alert('ERROR:\nYou are currently viewing this page through the file:/// scheme\nIn order for the page to function correctly, you must open it through a localhost.\nThis is because the JS "fetch()" function only works with http:// scheme.')
+}
+
+
+// 3. Navbar Loader =======================================
 // Load navbar into element with id #navbar 
 const navbar = document.getElementById('navbar');
 
-getComponent('navbar.html').then(component => {
-    navbar.innerHTML = component;
-}).then(() => {
-    // Set current page's nav link font to bold
-    const navLinks = document.querySelectorAll('.sk-nav-link');
-    const path = window.location.pathname.toLowerCase();
-
-    // Pesky edge case (default route)
-    if (path === '/') {
-        navLinks[0].classList.add('sk-nav-active');
-        return;
-    }
-
-    for (let i = 0; i <= navLinks.length; i++) {
-        console.log(navLinks[i].getAttribute('href').toLowerCase());
-        if (path === navLinks[i].getAttribute('href').toLowerCase()) {
-            navLinks[i].classList.add('sk-nav-active');
+if (navbar) {
+    getComponent('navbar.html').then(component => {
+        navbar.innerHTML = component;
+    }).then(() => {
+        // Set current page's nav link font to bold
+        const navLinks = document.querySelectorAll('.sk-nav-link');
+        const path = window.location.pathname.toLowerCase();
+    
+        // Pesky edge case (default route)
+        if (path === '/') {
+            navLinks[0].classList.add('sk-nav-active');
+            return;
         }
-    }
-});
+    
+        for (let i = 0; i <= navLinks.length; i++) {
+            if (path === navLinks[i].getAttribute('href').toLowerCase()) {
+                navLinks[i].classList.add('sk-nav-active');
+            }
+        }
+    });
+}
 
 
